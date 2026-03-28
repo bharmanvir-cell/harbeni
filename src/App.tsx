c3366d02011dc41f7ed480af68c6a92683a938c1
bharmanvir-cell <bharmanvir@gmail.com>
2026-03-28 15:50:15 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+ */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
@@ -12,22 +13,21 @@ import { NotFound } from './pages/NotFound';
 import { ScrollToTop } from './components/ScrollToTop';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="expertise" element={<Expertise />} />
-          <Route path="work" element={<Work />} />
-          <Route path="process" element={<Process />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="expertise" element={<Expertise />} />
+          <Route path="work" element={<Work />} />
+          <Route path="process" element={<Process />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
